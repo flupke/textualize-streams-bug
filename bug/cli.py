7ac096e6dc700ab3890b2378cde8898ee6a04df4
Luper Rouch <luper.rouch@gmail.com>
2023-06-28 19:55:09 +0200

    Implement counter example with Textualize

--- bug/cli.py
+++ bug/cli.py
@@ -1,12 +1,30 @@
+from typing import Callable, Any
 import asyncio
 
+from textual.app import App
+from textual.widgets import TextLog
 
-async def run_process(*args):
+
+async def run_process(*args, print_function: Callable[[str], Any]):
     proc = await asyncio.create_subprocess_exec(*args, stdout=asyncio.subprocess.PIPE)
     if proc.stdout is not None:
         async for line in proc.stdout:
-            print(line.decode().strip())
+            print_function(line.decode().strip())
 
 
 def counter():
-    asyncio.run(run_process("python", "counter.py"))
+    asyncio.run(run_process("python", "counter.py", print_function=print))
+
+
+class CounterApp(App):
+    def compose(self):
+        yield TextLog()
+
+    def on_ready(self):
+        view = self.query_one(TextLog)
+        self.run_worker(run_process("python", "counter.py", print_function=view.write))
+
+
+def gui_counter():
+    app = CounterApp()
+    app.run()
