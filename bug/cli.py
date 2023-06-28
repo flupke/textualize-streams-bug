2c0c2a08dfcce6d0ad868cb9adee050dcc976616
Luper Rouch <luper.rouch@gmail.com>
2023-06-28 20:26:54 +0200

    Add a phoenix app example
    Just run a phoenix app in a subprocess and poll its index.

--- bug/cli.py
+++ bug/cli.py
@@ -1,12 +1,19 @@
 from typing import Callable, Any
 import asyncio
 
+import aiohttp
+import aiohttp.client_exceptions
 from textual.app import App
 from textual.widgets import TextLog
 
 
-async def run_process(*args, print_function: Callable[[str], Any]):
-    proc = await asyncio.create_subprocess_exec(*args, stdout=asyncio.subprocess.PIPE)
+PrintFunction = Callable[[str], Any]
+
+
+async def run_process(*args, print_function: PrintFunction, cwd=None):
+    proc = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, cwd=cwd
+    )
     if proc.stdout is not None:
         async for line in proc.stdout:
             print_function(line.decode().strip())
@@ -28,3 +35,38 @@ class CounterApp(App):
 def gui_counter():
     app = CounterApp()
     app.run()
+
+
+async def run_phoenix(print_function: PrintFunction):
+    await run_process(
+        "mix", "deps.get", print_function=print_function, cwd="phoenix_app"
+    )
+    await run_process(
+        "mix", "assets.setup", print_function=print_function, cwd="phoenix_app"
+    )
+    await run_process(
+        "mix", "phx.server", print_function=print_function, cwd="phoenix_app"
+    )
+
+
+async def run_poller(print_function: PrintFunction):
+    timeout = aiohttp.ClientTimeout(total=5)
+    while True:
+        try:
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                while True:
+                    async with session.get("http://localhost:4000") as response:
+                        status = response.status
+                        print_function(f"--- Got reply: {status}")
+                        await asyncio.sleep(1)
+        except aiohttp.client_exceptions.ClientConnectorError:
+            print_function("Server not ready, retrying...")
+            await asyncio.sleep(1)
+
+
+async def do_phoenix():
+    await asyncio.gather(*(run_phoenix(print), run_poller(print)))
+
+
+def phoenix():
+    asyncio.run(do_phoenix())
